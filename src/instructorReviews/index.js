e52339d61dbcdeadd3af7e870c13ebcf3b490f17
bzwyant <bzwyant@gmail.com>
2023-11-08 09:54:16 -0500

    updated structure

---
+++ src/instructorReviews/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function loadCommentsButton() {
+    return(
+        <button>
+            Enter
+        </button>
+    );
+}
+
+export default function InstructorReviewsComponent() {
+    return(
+        <>
+        <h4>Reviews</h4>
+        <loadCommentsButton />
+        </>
+    );
+}
