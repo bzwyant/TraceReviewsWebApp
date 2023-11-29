8d2ecfeaaaeef21413c7607c5d6d9963fb18fa85
bzwyant <bzwyant@gmail.com>
2023-11-29 17:35:45 -0500

    query hooks branch

--- src/instructorReviews/index.js
+++ src/instructorReviews/index.js
@@ -1,18 +1,51 @@
-import React from "react"
+import React from 'react';
+import { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-function loadCommentsButton() {
+function LoadCommentsButton() {
+    const [reviews, setReviews] = useState([])
+
+    function queryReviews({instructor}) {
+
+    }
+
     return(
-        <button>
+        <button onClick={queryReviews}>
             Enter
         </button>
     );
+}
+
+const GET_REVIEWS = gql`
+    query Query($instructorInput: InstructorInput!r) {
+        getCourseComments(instructorInput: $instructorInput) {
+            termTitle
+            subject
+            number
+            comments
+        }
+    }
+`
+
+function InstructorReviews() {
+    const [inputText, setInputText] = useState('');
+
+    const { loading, error, data } = useQuery(GET_REVIEWS);
+
+    if (loading) return <p>'Loading...'</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+
+    return (
+        <form onSubmit={setInputText}></form>
+    )
 }
 
 export default function InstructorReviewsComponent() {
     return(
         <>
         <h4>Reviews</h4>
-        <loadCommentsButton />
+        <LoadCommentsButton />
         </>
     );
 }
